8adee591fa5f2ad62d1fd46fa92e0dcee7ad072d
JoeD016 <joemeliodasdong@gmail.com>
2023-01-30 22:31:08 -0500

    finish day6

---
+++ day6/part1.py
@@ -0,0 +1,38 @@
+import math
+import collections
+
+# import inbuilt standard input output
+from sys import stdin, stdout
+
+# suppose a function called main() and
+# all the operations are performed
+def main():
+
+    # input via .txt file
+    with open('input1.txt') as f:
+        lines = f.readlines()
+
+    input = lines[0]
+    left, right = 0, 3
+    cache = collections.defaultdict(int)
+    for i in range(left, 4):
+        cache[input[i]] += 1
+    
+    while right < len(input) - 1:
+        if len(cache) == 4:
+            print(right + 1)
+            return 
+        cache[input[left]] -= 1
+        if cache[input[left]] == 0:
+            del cache[input[left]]
+        right += 1
+        cache[input[right]] += 1
+        left += 1
+    return 
+
+        
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ day7/part1.py
@@ -0,0 +1,19 @@
+import math
+import collections
+
+# import inbuilt standard input output
+from sys import stdin, stdout
+
+# suppose a function called main() and
+# all the operations are performed
+def main():
+
+    # input via .txt file
+    with open('input1.txt') as f:
+        lines = f.readlines()
+
+    
+
+
+if __name__ == "__main__":
+    main()
